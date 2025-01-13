477c4e0a560cb12a5fbf1aa99511ca338c60a320
kiminyoung0628 <152356822+kiminyoung0628@users.noreply.github.com>
2025-01-13 16:36:36 +0900

    Feat : 프로젝트 구조 설계

---
+++ EndingCredits-api/src/test/java/com/hanaro/endingcredits/endingcreditsapi/EndingCreditsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hanaro.endingcredits.endingcreditsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EndingCreditsApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
