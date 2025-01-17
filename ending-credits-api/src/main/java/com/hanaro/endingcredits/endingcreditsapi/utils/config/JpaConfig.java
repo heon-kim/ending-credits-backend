d960636d95c9abdbe202d57bc647e836e768e008
kiminyoung0628 <152356822+kiminyoung0628@users.noreply.github.com>
2025-01-17 14:06:25 +0900

    ✨ FEAT. 컨트롤러 응답값 통일

--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/utils/config/JpaConfig.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/utils/config/JpaConfig.java
@@ -5,7 +5,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
 @EnableJpaRepositories(
-        basePackages = "com.hanaro.endingcredits.endingcreditsapi.domain.product.repository.jpa"
+        basePackages = {
+                "com.hanaro.endingcredits.endingcreditsapi.domain.product.repository.jpa",
+                "com.hanaro.endingcredits.endingcreditsapi.domain.member.repository"
+        }
 )
 public class JpaConfig {
     // JPA 관련 추가 설정이 필요한 경우 여기에 추가 가능
