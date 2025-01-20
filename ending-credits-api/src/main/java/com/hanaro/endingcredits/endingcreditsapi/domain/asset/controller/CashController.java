49e1c295ab3d35b420d8084078c04ad662670928
Sujin Lim <dkssudtnwls4794@naver.com>
2025-01-20 20:42:43 +0900

    ✨ FEAT. 현금 자산 조회 API 구현

--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/asset/controller/CashController.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/asset/controller/CashController.java
@@ -1,4 +1,28 @@
 package com.hanaro.endingcredits.endingcreditsapi.domain.asset.controller;
 
+import com.hanaro.endingcredits.endingcreditsapi.domain.asset.service.CashService;
+import com.hanaro.endingcredits.endingcreditsapi.utils.apiPayload.ApiResponseEntity;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/asset")
 public class CashController {
+
+    private final CashService cashService;
+
+    @GetMapping("/cash")
+    @Operation(summary= "현금 자산 조회", description = "보유하고 있는 현금 자산을 조회합니다.")
+    public ApiResponseEntity<BigDecimal> getMemberCash(@AuthenticationPrincipal UUID memberId) {
+        BigDecimal cash = cashService.getCashBalanceByMemberId(memberId);
+        return ApiResponseEntity.onSuccess("현금 자산 조회에 성공하였습니다", cash);
+    }
 }
--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/asset/service/CashService.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/asset/service/CashService.java
@@ -1,4 +1,33 @@
 package com.hanaro.endingcredits.endingcreditsapi.domain.asset.service;
 
+import com.hanaro.endingcredits.endingcreditsapi.domain.asset.entities.etc.CashEntity;
+import com.hanaro.endingcredits.endingcreditsapi.domain.asset.repository.etc.CashRepository;
+import com.hanaro.endingcredits.endingcreditsapi.domain.member.entities.MemberEntity;
+import com.hanaro.endingcredits.endingcreditsapi.domain.member.repository.MemberRepository;
+import com.hanaro.endingcredits.endingcreditsapi.utils.apiPayload.code.status.ErrorStatus;
+import com.hanaro.endingcredits.endingcreditsapi.utils.apiPayload.exception.handler.AssetHandler;
+import com.hanaro.endingcredits.endingcreditsapi.utils.apiPayload.exception.handler.MemberHandler;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
 public class CashService {
+    private final MemberRepository memberRepository;
+    private final CashRepository cashRepository;
+
+    public BigDecimal getCashBalanceByMemberId(UUID memberId) {
+        MemberEntity memberEntity = memberRepository.findByMemberId(memberId)
+                        .orElseThrow(() -> new MemberHandler(ErrorStatus.MEMBER_NOT_FOUND));
+
+        CashEntity cashEntity = cashRepository.findByAsset_Member_MemberId(memberId)
+                .orElseThrow(() -> new AssetHandler(ErrorStatus.CASH_NOT_FOUND));
+        return cashEntity.getAmount();
+    }
 }
