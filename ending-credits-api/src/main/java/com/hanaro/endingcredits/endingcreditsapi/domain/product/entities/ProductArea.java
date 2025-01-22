4a0a371f641f98fab9b293133754b54e62820f23
Sujin Lim <dkssudtnwls4794@naver.com>
2025-01-22 09:51:15 +0900

    📝 FIX. 퇴직연금 수익률 Entity 수정

--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/product/entities/ProductArea.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/product/entities/ProductArea.java
@@ -5,14 +5,17 @@ import java.util.Map;
 
 public enum ProductArea {
     BANK("은행", 1),
+    STOCK("증권", 2),
     ASSET_MANAGEMENT("자산운용", 3),
     LIFE_INSURANCE("생명보험", 4),
     GENERAL_INSURANCE("손해보험", 5);
 
+    private static final Map<String, ProductArea> descriptionToAreaMap = new HashMap<>();
     private static final Map<Integer, ProductArea> codeToAreaMap = new HashMap<>();
 
     static {
         for (ProductArea area : ProductArea.values()) {
+            descriptionToAreaMap.put(area.description, area);
             codeToAreaMap.put(area.code, area);
         }
     }
@@ -35,5 +38,9 @@ public enum ProductArea {
 
     public static ProductArea fromCode(int code) {
         return codeToAreaMap.getOrDefault(code, null);
+    }
+
+    public static ProductArea fromDescription(String description) {
+        return descriptionToAreaMap.getOrDefault(description, null);
     }
 }
--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/product/entities/RetirementPensionYieldEntity.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/product/entities/RetirementPensionYieldEntity.java
@@ -7,38 +7,37 @@ import lombok.*;
 import java.util.List;
 import java.util.Map;
 import java.util.UUID;
-import java.math.BigDecimal;
 
 @Entity
 @Getter
 @Setter(AccessLevel.PROTECTED)
-@Table(name = "retirement_pension_product")
+@Table(name = "retirement_pension_yield")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Builder
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class RetirementPensionProductEntity {
+public class RetirementPensionYieldEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
-    @Column(name="product_id")
-    private UUID productId;
+    @Column(name="company_yield_id")
+    private UUID companyYieldId;
 
     @Column(name="company", nullable = false, length = 50)
     private String company;
 
     @Enumerated(EnumType.STRING)
-    @Column(name="product_area", nullable = false)
-    private ProductArea productArea;
+    @Column(name="area", nullable = false)
+    private ProductArea area;
 
     @Enumerated(EnumType.STRING)
     @Column(name="sys_type", nullable = false)
     private SysType sysType;
 
-    @Column(name="product_detail", length = 10000)
+    @Column(name="yield_detail", length = 10000)
     @Convert(converter = JsonListConverter.class)
-    private List<Map<String, Object>> productDetail;
+    private List<Map<String, Object>> yieldDetail;
 
-    public List<Map<String, Object>> getProductDetail() {
-        return this.productDetail;
+    public List<Map<String, Object>> getYieldDetail() {
+        return this.yieldDetail;
     }
 }
