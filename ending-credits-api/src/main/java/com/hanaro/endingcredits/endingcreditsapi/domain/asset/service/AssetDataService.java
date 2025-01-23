c8742df35038a6a96a62c9cbaa18791356252178
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-01-23 14:51:00 +0900

    🔥 FIX. 자산의 은행, 저축은행, 거래소, 증권사 이름을 한글로 변경

--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/asset/service/AssetDataService.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/domain/asset/service/AssetDataService.java
@@ -100,12 +100,22 @@ public class AssetDataService {
     }
 
     private List<BankEntity> createBanks() {
-        String[] bankNames = {"Hana Bank", "Shinhan Bank", "NH Bank", "Woori Bank", "SC Bank", "KB Bank", "IM Bank", "City Bank", "Kakao Bank"};
+        String[] bankNames = {"KB국민은행", "카카오뱅크", "신한은행", "NH농협은행", "지역농협",
+                "하나은행", "우리은행", "IBK기업은행", "케이뱅크", "새마을금고",
+                "우체국", "신협", "SC제일은행", "iM뱅크", "BNK부산은행",
+                "BNK경남은행", "광주은행", "전북은행", "수협은행", "수협중앙회",
+                "씨티은행", "제주은행", "KDB산업은행", "산림조합중앙회",
+                "한국수출입은행", "한국농수한식품유통공사", "한국장학재단",
+                "한국주택금융공사", "신용회복위원회", "서민금융진흥원"};
         return createBankEntities(bankNames, false);
     }
 
     private List<BankEntity> createSavingsBanks() {
-        String[] savingsBankNames = {"DB Bank", "JT Bank", "Namyang Bank", "BNK Bank", "GuemHwa Bank", "DH Bank", "IBK Bank", "CK Bank", "Daebek Bank"};
+        String[] savingsBankNames = {"SBI저축은행", "OK저축은행", "웰컴저축은행", "신한저축은행",
+                "KB저축은행", "페퍼저축은행", "다올저축은행", "애큐온저축은행",
+                "하나저축은행", "NH저축은행", "한국투자저축은행", "상상인저축은행",
+                "IBK저축은행", "우리금융저축은행", "BNK저축은행", "고려저축은행",
+                "국제저축은행", "금화저축은행", "남양저축은행", "대명저축은행"};
         return createBankEntities(savingsBankNames, true);
     }
 
@@ -122,7 +132,7 @@ public class AssetDataService {
     }
 
     private List<ExchangeEntity> createExchanges() {
-        String[] exchangeNames = {"Upbit", "BitSum", "CoinOne", "Cobit", "GoPacks", "Pemex", "HuobiWeb", "FoblGate", "FlyBit"};
+        String[] exchangeNames = {"업비트", "빗썸", "코인원", "코빗", "고팍스"};
         List<ExchangeEntity> exchanges = new ArrayList<>();
         for (String name : exchangeNames) {
             ExchangeEntity exchange = ExchangeEntity.builder()
@@ -134,7 +144,14 @@ public class AssetDataService {
     }
 
     private List<SecuritiesCompanyEntity> createSecuritiesCompanies() {
-        String[] securitiesCompanyNames = {"KB Securities", "NH Securities", "SC Securities", "SK Securities", "Kyobo Securities", "GoldenBridge Securities", "Mirae Asset Securities", "Daesin Securities", "Kium Securities"};
+        String[] securitiesCompanyNames = {"한국투자증권", "키움증권", "미래에셋증권", "신한투자증권",
+                "NH투자증권", "KB증권", "삼성증권", "카카오페이증권", "하나증권",
+                "대신증권", "유안타증권", "한화투자증권", "DB금융투자",
+                "유진투자증권", "SK증권", "현대차증권", "IBK투자증권",
+                "하이투자증권", "신영증권", "LS증권", "우리종합금융",
+                "한국포스증권", "메리츠증권", "교보증권", "다올투자증권",
+                "코리아에셋투자증권", "BNK투자증권", "케이프투자증권",
+                "한국증권금융", "부국증권"};
         List<SecuritiesCompanyEntity> companies = new ArrayList<>();
         for (String name : securitiesCompanyNames) {
             SecuritiesCompanyEntity company = SecuritiesCompanyEntity.builder()
