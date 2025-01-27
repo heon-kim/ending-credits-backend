a9ccdcb1d43938b58fccb3ff124b257ba3a40457
Sujin Lim <dkssudtnwls4794@naver.com>
2025-01-27 14:38:17 +0900

    ⚡️ FIX. 부동산 자산 조회 시 타입 추가

--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/utils/apiPayload/code/status/ErrorStatus.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/utils/apiPayload/code/status/ErrorStatus.java
@@ -40,6 +40,7 @@ public enum ErrorStatus implements BaseErrorCode {
 
     // 자산 관련 에러
     ASSET_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "ASSET4001", "존재하지 않는 자산 유형입니다."),
+    REAL_ESTATE_TYPE_NOT_FOUND(HttpStatus.BAD_REQUEST, "ASSET4001", "존재하지 않는 부동산 자산 유형입니다."),
 
     CASH_NOT_FOUND(HttpStatus.BAD_REQUEST, "CASH4001", "현금 자산이 존재하지 않습니다."),
     NEGATIVE_AMOUNT_NOT_ALLOWED(HttpStatus.BAD_REQUEST, "CASH4002", "금액은 음수일 수 없습니다."),
