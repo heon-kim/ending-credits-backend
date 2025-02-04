05d99f561227c488de9432b728db0dd403c58556
insoo00 <insoosos1234@gmail.com>
2025-02-04 14:16:43 +0900

    :twisted_rightwards_arrows: es 설정 관련 병합

--- ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/utils/config/ElasticSearchConfig.java
+++ ending-credits-api/src/main/java/com/hanaro/endingcredits/endingcreditsapi/utils/config/ElasticSearchConfig.java
@@ -1,14 +1,7 @@
 package com.hanaro.endingcredits.endingcreditsapi.utils.config;
 
-import co.elastic.clients.elasticsearch.ElasticsearchClient;
-import co.elastic.clients.transport.rest_client.RestClientTransport;
-import org.apache.http.HttpHost;
-import org.elasticsearch.client.RestClient;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
-import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate;
-import org.springframework.data.elasticsearch.core.ElasticsearchOperations;
 import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 
@@ -21,20 +14,7 @@ public class ElasticSearchConfig extends ElasticsearchConfiguration {
     @Override
     public ClientConfiguration clientConfiguration() {
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
-                .withBasicAuth("elastic", "endingcredits") // Elasticsearch 인증 정보
+                .connectedTo("es:9200")
                 .build();
-    }
-
-    @Bean
-    public ElasticsearchClient elasticsearchClient() {
-        RestClient restClient = RestClient.builder(HttpHost.create("http://localhost:9200")).build();
-        RestClientTransport transport = new RestClientTransport(restClient, new co.elastic.clients.json.jackson.JacksonJsonpMapper());
-        return new ElasticsearchClient(transport);
-    }
-
-    @Bean
-    public ElasticsearchOperations elasticsearchOperations() {
-        return new ElasticsearchTemplate(elasticsearchClient());
     }
 }
